62a7811dba9298cb2f4740d964129d13ca99bf19
TranYen103 <yen1032005@gmail.com>
2025-03-14 01:02:32 +0700

    update all2

---
+++ app/src/main/java/com/example/do_an/fragments/HomeFragment.java
@@ -0,0 +1,39 @@
+package com.example.do_an.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.do_an.FoodAdapter;
+import com.example.do_an.FoodItem;
+import com.example.do_an.R;
+
+import java.util.ArrayList;
+
+
+public class HomeFragment extends Fragment {
+
+
+
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+
+
+        return  view;
+    }
+
+}
