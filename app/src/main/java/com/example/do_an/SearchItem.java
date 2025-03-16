d97e4145cfec2916ee5c38811a10caceee9178ba
Nhã Trân <jacksonwang0705@gmail.com>
2025-03-16 21:14:09 +0700

    update

---
+++ app/src/main/java/com/example/do_an/SearchItem.java
@@ -0,0 +1,30 @@
+package com.example.do_an;
+
+public class SearchItem {
+    String foodName, foodTime;
+    int img;
+
+    public int getImg() {
+        return img;
+    }
+
+    public void setImg(int img) {
+        this.img = img;
+    }
+
+    public String getFoodTime() {
+        return foodTime;
+    }
+
+    public void setFoodTime(String foodTime) {
+        this.foodTime = foodTime;
+    }
+
+    public String getFoodName() {
+        return foodName;
+    }
+
+    public void setFoodName(String foodName) {
+        this.foodName = foodName;
+    }
+}
